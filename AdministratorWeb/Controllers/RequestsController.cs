f61a3755e3b6a11f3ed605c0d315da6c7e251c52
JvJalandoni <s2120650@usls.edu.ph>
2025-11-15 14:26:56 +0800

    Add missing using for UserManager

--- AdministratorWeb/Controllers/RequestsController.cs
+++ AdministratorWeb/Controllers/RequestsController.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity;
 using AdministratorWeb.Data;
 using AdministratorWeb.Models;
 using AdministratorWeb.Models.DTOs;
